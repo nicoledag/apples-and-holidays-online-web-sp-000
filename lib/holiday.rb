ad3c8ab4f32ce4ff4e31f63dbb838871b7c7dc30
Nicole D <nnardella@gmail.com>
2018-11-03 00:41:20 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -23,10 +23,10 @@ def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_arr
 end
 
 def all_winter_holiday_supplies(holiday_hash)
-  holiday_hash[:winter].map do |holiday, supplies|
-    supplies
-  end.flatten
-#holiday_hash[:winter].values.flatten
+  # holiday_hash[:winter].map do |holiday, supplies|
+  #   supplies
+  # end.flatten
+holiday_hash[:winter].values.flatten
 end
 
 def all_supplies_in_holidays(holiday_hash)
