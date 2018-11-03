e3ed14bb9beeb1c644305e25ad0fe520c3c6cb45
Nicole D <nnardella@gmail.com>
2018-11-03 01:47:43 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -43,6 +43,9 @@ end
 
 
 def all_holidays_with_bbq(holiday_hash)
-
-
+    holiday_hash.map do |season, holidays|
+      holidays.map do |holiday_name, supplies|
+        supplies.include?("BBQ") ? holiday_name : nil
+      end
+   end.flatten.compact
 end
